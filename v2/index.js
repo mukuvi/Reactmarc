41532b714b43c626bf7b7bac5be1d45e03b6a1cd
mukuvi <jamesmngandu@gmail.com>
2025-04-04 23:07:01 +0300

    reactmarc

--- v2/index.js
+++ v2/index.js
@@ -6,62 +6,60 @@
 
 /* == UI - Elements == */
 
-const viewLoggedOut = document.getElementById("logged-out-view");
-const viewLoggedIn = document.getElementById("logged-in-view");
+const viewLoggedOut = document.getElementById("logged-out-view")
+const viewLoggedIn = document.getElementById("logged-in-view")
 
-const signInWithGoogleButtonEl = document.getElementById(
-  "sign-in-with-google-btn"
-);
+const signInWithGoogleButtonEl = document.getElementById("sign-in-with-google-btn")
 
-const emailInputEl = document.getElementById("email-input");
-const passwordInputEl = document.getElementById("password-input");
+const emailInputEl = document.getElementById("email-input")
+const passwordInputEl = document.getElementById("password-input")
 
-const signInButtonEl = document.getElementById("sign-in-btn");
-const createAccountButtonEl = document.getElementById("create-account-btn");
+const signInButtonEl = document.getElementById("sign-in-btn")
+const createAccountButtonEl = document.getElementById("create-account-btn")
 
 /* == UI - Event Listeners == */
 
-signInWithGoogleButtonEl.addEventListener("click", authSignInWithGoogle);
+signInWithGoogleButtonEl.addEventListener("click", authSignInWithGoogle)
 
-signInButtonEl.addEventListener("click", authSignInWithEmail);
-createAccountButtonEl.addEventListener("click", authCreateAccountWithEmail);
+signInButtonEl.addEventListener("click", authSignInWithEmail)
+createAccountButtonEl.addEventListener("click", authCreateAccountWithEmail)
 
 /* === Main Code === */
 
-showLoggedOutView();
+showLoggedInView()
 
 /* === Functions === */
 
 /* = Functions - Firebase - Authentication = */
 
 function authSignInWithGoogle() {
-  console.log("Sign in with Google");
+    console.log("Sign in with Google")
 }
 
 function authSignInWithEmail() {
-  console.log("Sign in with email and password");
+    console.log("Sign in with email and password")
 }
 
 function authCreateAccountWithEmail() {
-  console.log("Sign up with email and password");
+    console.log("Sign up with email and password")
 }
 
 /* == Functions - UI Functions == */
 
 function showLoggedOutView() {
-  hideElement(viewLoggedIn);
-  showElement(viewLoggedOut);
+    hideElement(viewLoggedIn)
+    showElement(viewLoggedOut)
 }
 
 function showLoggedInView() {
-  hideElement(viewLoggedOut);
-  showElement(viewLoggedIn);
+    hideElement(viewLoggedOut)
+    showElement(viewLoggedIn)
 }
 
 function showElement(element) {
-  element.style.display = "flex";
+    element.style.display = "flex"
 }
 
 function hideElement(element) {
-  element.style.display = "none";
+    element.style.display = "none"
 }
